dd08db60807b34034d190d6fee2efa20d20fb75d
dina <andrianambinintsoadina@Gmail.com>
2025-12-20 13:13:58 +0300

    restoring modified file

--- dashboard.r
+++ dashboard.r
@@ -1,48 +1,40 @@
-# Dashboard app for discharge monitoring 
-# Code written by ANDRIANAMBININTSOA Dina Larry Ismael 
-#library importation
+ library("writexl")
+ library("shiny")
+ library("shinydashboard")
+library("dplyr")
 library("ggplot2")
 library("readxl")
-library("dplyr")
 library("lubridate")
-library("writexl")
-library("shiny")
-library("shinydashboard")
-
-#UI creation
-ui = dashboardPage(
-    dashboardHeader(title = "Discharge analysis dashboard"),
-    dashboardSidebar(
+ #UI creation  
+ ui = dashboardPage(
+     dashboardHeader(title = "Discharge analysis dashboard"),
+     dashboardSidebar(
         sidebarMenu(
-            menuItem("Upload files",icon = icon("upload"),fileInput("dataset","Enter here your file:",accept = c(".xlsx",".xls",".csv"))),
-            menuItem("Monthly discharge",tabName = "tab1",icon = icon("chart-bar")),
-            menuItem("Daily discharge",tabName = "tab2",icon = icon("chart-bar")),
+            menuItem("Upload file",fileInput("dataset","Upload here a dataset",accept = c(".xls",".xlsx",".csv"))),
+            menuItem("Discharge Summary",tabName = "tab1",icon = icon("chart-bar")),
+            menuItem("Discharge per Month",tabName = "tab2",icon = icon("chart-bar")),
             menuItem("Hydrogramm per day",tabName = "tab3",icon = icon("chart-line"))
         )
-    ),
-    dashboardBody(
-        tabItems(
-           tabItem(tabName = "tab1", fluidRow(valueBoxOutput("monthlymin",width = 4),valueBoxOutput("monthlymean",width = 4),valueBoxOutput("monthlymax",width = 4)),
-           fluidRow(box(width = 7,title = "Monthly discharge",plotOutput("monthly",height = 40),downloadButton("down1","Download plot")),
+     ),
+     dashboardBody(
+         tabItems(
+            tabItem(tabName = "tab1", fluidRow(valueBoxOutput("monthlymin",width = 4),valueBoxOutput("monthlymean",width = 4),valueBoxOutput("monthlymax",width = 4)),
+          fluidRow(box(width = 7,title = "Monthly discharge",plotOutput("monthly",height = 40),downloadButton("down1","Download plot")),
            box(width = 5,title = "Monthly peak discharge",plotOutput("monthly_peak",height = 40),downloadButton("down2","Download plot")),
-           box(width = 4,title = "Control",sliderInput("month_size","Select sample size :",min = 1,max = 12,value = 12)))
-           ),
-           tabItem(tabName = "tab2",fluidRow(valueBoxOutput("dailylymin",width = 4),valueBoxOutput("daylymean",width = 4),valueBoxOutput("dailylymax",width = 4)),
-           fluidRow(box(width = 7,title = "daily discharge",plotOutput("daily",height = 40),downloadButton("down3","Download plot")),
-           box(width = 5,title = "daily peak discharge",plotOutput("daily_peak",height = 40),downloadButton("down4","Download plot")),
-           box(width = 4,sliderInput("day_size","Select sample size :",min = 1,max = 31,value = 15)))
-           ),
-           tabItem(tabName = "tab3",fluidRow(valueBoxOutput("dischargemin",width = 40),valueBoxOutput("dischargemean",width = 4),valueBoxOutput("dischargemax",width = 4)),
+           box(width = 4,sliderInput("month_size","Select sample size :",min = 1,max = 12,value = 12)))),
+          tabItem(tabName = "tab2",fluidRow(valueBoxOutput("dailylymin",width = 4),valueBoxOutput("daylymean",width = 4),valueBoxOutput("dailylymax",width = 4)),
+         fluidRow(box(width = 7,title = "daily discharge",plotOutput("daily",height = 40),downloadButton("down3","Download plot")),
+         box(width = 5,title = "daily peak discharge",plotOutput("daily_peak",height = 40),downloadButton("down4","Download plot")),
+            box(width = 4,sliderInput("day_size","Select sample size :",min = 1,max = 31,value = 15)))
+            ),
+            tabItem(tabName = "tab3",fluidRow(valueBoxOutput("dischargemin",width = 40),valueBoxOutput("dischargemean",width = 4),valueBoxOutput("dischargemax",width = 4)),
            fluidRow(box(width = 7,title = "Hydrogramm",plotOutput("hydro"),downloadButton("down5","Download plot")),
-           box(width = 5,dateRangeInput("period","Select datetime size :",format = "yyyy-mm-dd",start = Sys.Date() - 30,end = Sys.Date()),
-           actionButton("refresh","Refresh date",class = "btn - success")),
-           infoBoxOutput("peak",width = 4),
-           ))
-           ), 
-        )
-    )
-   
-
+            box(width = 5,dateRangeInput("period","Select datetime size :",format = "yyyy-mm-dd",start = Sys.Date() - 30,end = Sys.Date()),
+            actionButton("refresh","Refresh date",class = "btn - success")),
+            infoBoxOutput("peak",width = 4),)))
+         
+     )
+     )
 server = function(input,output){
 sheets = reactive({sheets = c("Janv_02","Fév_02","Mars_02","Avril_02","Mai_02","Juin_02","Juil_02","Août_02","Sept_02","Oct_02","Nov_02","Déc_02")
 return(sheets)})
@@ -75,7 +67,7 @@ req(input$dataset)
 valueBox(width = 5,paste("Mean for",input$month_size," months"),mean(data_mean_month()[1:input$month_size]),)
 })
 data_max_month = reactive({
-
+ 
 max_discharge = c(rep(0,12))
 for (i in 1:length(sheets())){
 data = read_excel(input$dataset$datapath,sheet = sheets()[i])
@@ -88,5 +80,5 @@ output$monthlymax = renderValueBox({
 req(input$dataset)
 valueBox(width = 4,paste("Max for ",input$month_size," months"),mean(data_max_month()[1:input$month_size]),color = "red")
 })
-}
-shinyApp(ui,server)
+ }
+ shinyApp(ui,server)
